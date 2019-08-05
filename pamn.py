9af9361386de00aea813def48240731c31295130
Ryuichi Ueno <ryu1@stu.kobe-u.ac.jp>
2019-08-05 12:04:27 -0500

    set default for arguments

--- pamn.py
+++ pamn.py
@@ -3,21 +3,9 @@ import subprocess
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input', help='init txt file listing paths to Sort output files')
-parser.add_argument('-o', '--output', help='pamn output root file')
+parser.add_argument('-i', '--input', default='init.txt', help='init txt file listing paths to Sort output files')
+parser.add_argument('-o', '--output', default='output.root', help='pamn output root file')
 args = parser.parse_args()
 
-# init
-if args.input is not None:
-    init = args.input
-else:
-    init = 'init.txt'
-
-# output
-if args.output is not None:
-    output = args.output
-else:
-    output = 'output.root'
-
 pamn = './bin/pamn'
-subprocess.call([pamn, '-i', init, '-o', output])
+subprocess.call([pamn, '-i', args.input, '-o', args.utput])
