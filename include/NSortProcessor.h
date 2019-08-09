6f2606246473e6226a8317192698558093c67ca1
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2019-08-09 10:02:49 +0900

    Add systematical way to move and add new branchs

--- include/NSortProcessor.h
+++ include/NSortProcessor.h
@@ -19,35 +19,102 @@ public:
 
     void ActivateBranchs();
     void SetFileList();
+
+    void SetInputBranchs();
+
     void SetVerbose(int Verbose){this->Verbose = Verbose;};
+
     std::vector<TString> ifiles;
     int Verbose = 1;
 
 private:
-    // for input
-    TString m_init;
-    TFile* m_ifile;
-    TTree* m_itree;
-    Int_t ns;
-    Float_t X[3000];
-    Float_t Y[3000];
-    Float_t Z[3000];
-    Float_t NR[3000];
-    Float_t Ed[3000];
-    Float_t S2[3000];
-    std::vector<Int_t>* pmthitid;
-
-    // for output
+    // For an output file
     TString m_output;
     TFile* m_ofile;
     TTree* m_otree;
-    Int_t m_pri;
-    Int_t m_ns;
+
+    // Output parameters
     Float_t m_fv;
-    Float_t m_NR;
-    Float_t m_Ed;
-    Float_t m_secondS2;
     Int_t m_nhits;
+
+
+    // For input files
+    TString m_init;
+    TFile* m_ifile;
+    TTree* m_itree;
+
+    Int_t           eventid;
+    Float_t         xpri;
+    Float_t         ypri;
+    Float_t         zpri;
+    Float_t         epri;
+    std::vector<std::string>  *typepri = nullptr;
+    std::string          *vpri = nullptr;
+    Float_t         S1;
+    Int_t           ns;
+    Float_t         X[40];   //[ns]
+    Float_t         Y[40];   //[ns]
+    Float_t         Z[40];   //[ns]
+    Float_t         NR[40];   //[ns]
+    Float_t         Ed[40];   //[ns]
+    Float_t         S2[40];   //[ns]
+    Float_t         cS2[40];   //[ns]
+    Float_t         Time[40];   //[ns]
+    Float_t         TotNrgTpc;
+    Float_t         TotNrgBc;
+    Float_t         TotNrgVeto;
+    Int_t           nFax;
+    Float_t         Fax_x[271];   //[nFax]
+    Float_t         Fax_y[271];   //[nFax]
+    Float_t         Fax_z[271];   //[nFax]
+    Float_t         Fax_t[271];   //[nFax]
+    Float_t         Fax_ed[271];   //[nFax]
+    Float_t         Fax_nr[271];   //[nFax]
+    Int_t           Fax_nph[271];   //[nFax]
+    Int_t           Fax_nel[271];   //[nFax]
+    Int_t           GasInteraction;
+    Int_t           npmthit;
+    std::vector<int>     *pmthitid = nullptr;
+    std::vector<double>  *pmthittime = nullptr;
+    std::vector<float>   *pmthitenergy = nullptr;
+
+    TBranch        *b_eventid;   //!
+    TBranch        *b_xpri;   //!
+    TBranch        *b_ypri;   //!
+    TBranch        *b_zpri;   //!
+    TBranch        *b_epri;   //!
+    TBranch        *b_typepri;   //!
+    TBranch        *b_vpri;   //!
+    TBranch        *b_S1;   //!
+    TBranch        *b_ns;   //!
+    TBranch        *b_X;   //!
+    TBranch        *b_Y;   //!
+    TBranch        *b_Z;   //!
+    TBranch        *b_NR;   //!
+    TBranch        *b_Ed;   //!
+    TBranch        *b_S2;   //!
+    TBranch        *b_cS2;   //!
+    TBranch        *b_Time;   //!
+    TBranch        *b_TotNrgTpc;   //!
+    TBranch        *b_TotNrgBc;   //!
+    TBranch        *b_TotNrgVeto;   //!
+    TBranch        *b_nFax;   //!
+    TBranch        *b_Fax_x;   //!
+    TBranch        *b_Fax_y;   //!
+    TBranch        *b_Fax_z;   //!
+    TBranch        *b_Fax_t;   //!
+    TBranch        *b_Fax_ed;   //!
+    TBranch        *b_Fax_nr;   //!
+    TBranch        *b_Fax_nph;   //!
+    TBranch        *b_Fax_nel;   //!
+    TBranch        *b_GasInteraction;   //!
+    TBranch        *b_npmthit;   //!
+    TBranch        *b_pmthitid;   //!
+    TBranch        *b_pmthittime;   //!
+    TBranch        *b_pmthitenergy;   //!
+
 };
+
+
 
 #endif
--- src/NSortProcessor.C
+++ src/NSortProcessor.C
@@ -16,8 +16,7 @@
 #include <algorithm>
 
 NSortProcessor::NSortProcessor(TString init, TString output):
-    m_init(init),
-    m_output(output)
+    m_output(output), m_init(init)
 {
     m_ofile = new TFile(m_output, "recreate");
     m_otree = new TTree("events", "process and integrate nSort resultsh");
@@ -33,17 +32,25 @@ void NSortProcessor::Init(){
     this->SetFileList();
     this->ActivateBranchs();
 }
+
+
 void NSortProcessor::ActivateBranchs(){
 
     // initialize the output tree
-    m_otree->Branch("ns", &m_ns);
+    // Copy branchs from the input files
+    m_otree->Branch("ns", &ns);
+    m_otree->Branch("NR", &NR[0]);
+    m_otree->Branch("Ed", &Ed[0]);
+    m_otree->Branch("secondS2", &S2[1]);
+    m_otree->Branch("pmthitid", &pmthitid);
+
+    // Calc variables in Process()
     m_otree->Branch("fv", &m_fv);
-    m_otree->Branch("NR", &m_NR);
-    m_otree->Branch("Ed", &m_Ed);
-    m_otree->Branch("secondS2", &m_secondS2);
     m_otree->Branch("nhits", &m_nhits);
 
 }
+
+
 void NSortProcessor::SetFileList(){
 
     // read the init
@@ -56,48 +63,98 @@ void NSortProcessor::SetFileList(){
         } else if (ifilename.at(0) == '#') {
             continue;
         } else {
-            ifiles.push_back(ifilename);
+            ifiles.emplace_back(ifilename);
         }
     }
 
 }
+
+
+void NSortProcessor::SetInputBranchs(){
+
+    m_itree->SetBranchAddress("eventid", &eventid, &b_eventid);
+    m_itree->SetBranchAddress("xpri", &xpri, &b_xpri);
+    m_itree->SetBranchAddress("ypri", &ypri, &b_ypri);
+    m_itree->SetBranchAddress("zpri", &zpri, &b_zpri);
+    m_itree->SetBranchAddress("epri", &epri, &b_epri);
+    m_itree->SetBranchAddress("typepri", &typepri, &b_typepri);
+    m_itree->SetBranchAddress("vpri", &vpri, &b_vpri);
+    m_itree->SetBranchAddress("S1", &S1, &b_S1);
+    m_itree->SetBranchAddress("ns", &ns, &b_ns);
+    m_itree->SetBranchAddress("X", X, &b_X);
+    m_itree->SetBranchAddress("Y", Y, &b_Y);
+    m_itree->SetBranchAddress("Z", Z, &b_Z);
+    m_itree->SetBranchAddress("NR", NR, &b_NR);
+    m_itree->SetBranchAddress("Ed", Ed, &b_Ed);
+    m_itree->SetBranchAddress("S2", S2, &b_S2);
+    m_itree->SetBranchAddress("cS2", cS2, &b_cS2);
+    m_itree->SetBranchAddress("Time", Time, &b_Time);
+    m_itree->SetBranchAddress("TotNrgTpc", &TotNrgTpc, &b_TotNrgTpc);
+    m_itree->SetBranchAddress("TotNrgBc", &TotNrgBc, &b_TotNrgBc);
+    m_itree->SetBranchAddress("TotNrgVeto", &TotNrgVeto, &b_TotNrgVeto);
+    m_itree->SetBranchAddress("nFax", &nFax, &b_nFax);
+    m_itree->SetBranchAddress("Fax_x", Fax_x, &b_Fax_x);
+    m_itree->SetBranchAddress("Fax_y", Fax_y, &b_Fax_y);
+    m_itree->SetBranchAddress("Fax_z", Fax_z, &b_Fax_z);
+    m_itree->SetBranchAddress("Fax_t", Fax_t, &b_Fax_t);
+    m_itree->SetBranchAddress("Fax_ed", Fax_ed, &b_Fax_ed);
+    m_itree->SetBranchAddress("Fax_nr", Fax_nr, &b_Fax_nr);
+    m_itree->SetBranchAddress("Fax_nph", Fax_nph, &b_Fax_nph);
+    m_itree->SetBranchAddress("Fax_nel", Fax_nel, &b_Fax_nel);
+    m_itree->SetBranchAddress("GasInteraction", &GasInteraction, &b_GasInteraction);
+    m_itree->SetBranchAddress("npmthit", &npmthit, &b_npmthit);
+    m_itree->SetBranchAddress("pmthitid", &pmthitid, &b_pmthitid);
+    m_itree->SetBranchAddress("pmthittime", &pmthittime, &b_pmthittime);
+    m_itree->SetBranchAddress("pmthitenergy", &pmthitenergy, &b_pmthitenergy);
+
+    m_itree->SetBranchStatus("*", 0);
+
+}
+
+
+
 void NSortProcessor::Process(){
     // loop for input files
     ULong64_t total_entries = 0;
     for (const TString & ifile : ifiles) {
         // display the current input file
-        std::cout << "-----> inputfile: " << ifile << std::endl;
+        if(Verbose>1) std::cout << "-----> inputfile: " << ifile << std::endl;
 
         // initialize the input tree
         m_ifile = TFile::Open(ifile, "read");
         m_ifile->cd("events");
         m_itree = new TTree();
         m_itree = dynamic_cast<TTree*>(gDirectory->Get("events"));
-        
-        m_itree->SetBranchAddress("ns", &ns);
-        m_itree->SetBranchAddress("X", X);
-        m_itree->SetBranchAddress("Y", Y);
-        m_itree->SetBranchAddress("Z", Z);
-        m_itree->SetBranchAddress("NR", NR);
-        m_itree->SetBranchAddress("Ed", Ed);
-        m_itree->SetBranchAddress("S2", S2);
-        m_itree->SetBranchAddress("pmthitid", &pmthitid);
+
+        this->SetInputBranchs();
+
+        // Activate used branchs
+        m_itree->SetBranchStatus("ns");
+        m_itree->SetBranchStatus("X");
+        m_itree->SetBranchStatus("Y");
+        m_itree->SetBranchStatus("Z");
+        m_itree->SetBranchStatus("NR");
+        m_itree->SetBranchStatus("Ed");
+        m_itree->SetBranchStatus("S2");
+        m_itree->SetBranchStatus("pmthitid");
 
         // loop for events
         const ULong64_t nentries = m_itree->GetEntries();
         total_entries += nentries;
         for (UInt_t ientry = 0; ientry < nentries; ++ientry) {
+            if(Verbose>2 && ientry % (nentries/10) == 0) std::cout << "File read " << (double)ientry/nentries*100 << "%" << std::endl;
             m_itree->GetEntry(ientry);
 
-            // nSort branches
-            m_ns = ns;
+
+            // ------------------------------------>>>
+            // FV
             const Float_t r2 = X[0] * X[0] + Y[0] * Y[0];
             const Float_t z_fv = Z[0] + 739.0;
             m_fv = TMath::Power(TMath::Abs(z_fv/629.0), 3.0) + TMath::Power(TMath::Abs(r2/396900.0), 3.0);
-            m_NR = NR[0];
-            m_Ed = Ed[0];
-            m_secondS2 = S2[1];
+            // <<<------------------------------------
 
+
+            // ------------------------------------>>>
             // nhits
             constexpr UInt_t nNV = 121;
             std::array<Float_t, nNV> cnt {0};
@@ -115,12 +172,13 @@ void NSortProcessor::Process(){
                 } else {
                     phe = 0.;
                 }
-
                 if (phe >= phe_threshold) {
                     ++nhits;
                 }
             }
             m_nhits = nhits;
+            // <<<------------------------------------
+
 
             // fill output tree
             m_otree->Fill();
@@ -132,9 +190,12 @@ void NSortProcessor::Process(){
     }
 
     // write the output tree into the output file
-    std::cout << "-----> " << total_entries << " events are written." << std::endl;
+    if(Verbose>1) std::cout << "-----> " << total_entries << " events are written." << std::endl;
 
 }
+
+
+
 void NSortProcessor::Terminate(){
     m_ofile->cd();
     m_otree->Write();
